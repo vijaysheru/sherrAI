74a4dfc90fd70faace3b1e644c7aad52654e0234
Vijay Sheru <124458353+vijaysheru@users.noreply.github.com>
2025-02-14 14:07:20 -0600

    Fixed 404 NOT FOUND Error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,15 @@
 module.exports = {
   reactStrictMode: true,
   async rewrites() {
+    if (!process.env.NEXT_PUBLIC_API_URL) {
+      console.error("❌ NEXT_PUBLIC_API_URL is not set!");
+      return [];
+    }
+
     return [
       {
         source: "/api/:path*",
-        destination: process.env.NEXT_PUBLIC_API_URL + "/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
